49bbeb2a75139a406143c8db447878d50b2a52f4
Maksim Khramov <maxtemplaar@gmail.com>
2024-07-07 00:46:30 +0300

    New implementation

---
+++ ArtOfIllusion/src/artofillusion/ui/PreviewWidget.java
@@ -0,0 +1,9 @@
+package artofillusion.ui;
+
+import buoy.widget.CustomWidget;
+
+public class PreviewWidget extends CustomWidget {
+    PreviewWidget() {
+        this.component = new PreviewComponent();
+    }
+}
